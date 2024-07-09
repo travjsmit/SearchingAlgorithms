8d17e4cf2bc0584c40a3f57fc6a6292607e80022
travjsmit <inivid@icloud.com>
2024-07-09 14:17:44 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace SearchingAlgorithms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr1 = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int arr1Result = LinearSearch(arr1, 4);
+            Console.WriteLine(arr1Result);
+        }
+
+        static int LinearSearch(int[] arrToSearch, int target)
+        {
+            //int i = 0 - initializing our loop conditional - in tthis case to 0
+            // next we evaluate if the value meets the condition
+            // after each loop we run the update logic
+            // Big O - O(n)
+            // Big Omega - O(1)
+            for (int i = 0; i < arrToSearch.Length; i++)
+            {
+                if (arrToSearch[i] == target) return i;
+            }
+
+            return -1;
+        }
+    }
+}
